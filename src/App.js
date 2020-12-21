43da1c1a08f6bd3a940b415a23854d87537146b7
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2020-12-21 18:12:24 +0330

    e17.2- simple jquery

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import $ from "jquery";
+import { findDOMNode } from "react-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const toggleBtn = useRef(null);
+  const jqFunc = () => {
+    $(findDOMNode(toggleBtn.current)).slideToggle();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img ref={toggleBtn} src={logo} className='App-logo' alt='logo' />
+        <br />
+        <button onClick={jqFunc}>click</button>
       </header>
     </div>
   );
